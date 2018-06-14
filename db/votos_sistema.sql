f9fce53691118e8c2874f5ad98ca4ed38d88f8d1
Matikosososo <matiasjsd@gmail.com>
2018-06-13 21:07:02 -0400

    inicio de archivo sql, falta terminarlo

---
+++ db/votos_sistema.sql
@@ -0,0 +1,31 @@
+create database votos_sistem;
+
+use votos_sistem;
+
+create table pais(
+
+);
+
+create table nacionalidad(
+
+);
+
+create table partido(
+
+);
+
+create table persona(
+
+);
+
+create table tipo_usuario(
+
+);
+
+create table candidato(
+
+);
+
+create table voto(
+
+);
