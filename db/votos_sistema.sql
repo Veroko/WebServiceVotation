d00ff63f5264ef51df03a0668bc401d40350beef
Veroko <veronica.alvarado.j97@gmail.com>
2018-06-14 11:40:15 -0400

    Base de datos lista

--- db/votos_sistema.sql
+++ db/votos_sistema.sql
@@ -3,29 +3,57 @@ create database votos_sistem;
 use votos_sistem;
 
 create table pais(
-
+    id INT AUTO_INCREMENT,
+    nombre_pais VARCHAR(50),
+    PRIMARY KEY(id)
 );
 
 create table nacionalidad(
-
+    id INT AUTO_INCREMENT,
+    nombre_nacionalidad VARCHAR(50),
+    PRIMARY KEY(id)
 );
 
 create table partido(
-
+    id INT AUTO_INCREMENT,
+    nombre_partido VARCHAR(50),
+    PRIMARY KEY(id)
 );
 
 create table persona(
-
+    id INT AUTO_INCREMENT,
+    rut INT UNIQUE,
+    nombre VARCHAR(100),
+    nacionalidad_fk int,
+    pais_fk int,
+    tipo_usuario_fk int,
+    sufragio boolean,
+    PRIMARY KEY(id),
+    FOREIGN KEY(nacionalidad_fk) REFERENCES nacionalidad(id),
+    FOREIGN KEY(pais_fk) REFERENCES pais(id),
+    FOREIGN KEY(tipo_usuario_fk) REFERENCES tipo_usuario(id)
 );
 
 create table tipo_usuario(
-
+    id INT AUTO_INCREMENT,
+    privilegio VARCHAR(50),
+    PRIMARY KEY(id)
 );
 
 create table candidato(
-
+    id INT AUTO_INCREMENT,
+    persona_fk INT UNIQUE,
+    partido_fk int,
+    PRIMARY KEY(id)
+    FOREIGN KEY(persona_fk) REFERENCES persona(rut),
+    FOREIGN KEY(partido_fk) REFERENCES partido(id)
 );
 
 create table voto(
-
+    id INT AUTO_INCREMENT,
+    persona_fk INT UNIQUE,
+    candidato_fk INT,
+    PRIMARY KEY(id),
+    FOREIGN KEY(persona_fk) REFERENCES persona(rut),
+    FOREIGN KEY(candidato_fk) REFERENCES candidato(id)
 );
